44430c10b4df3f1b8dd4fb7f8c56a512cff6a293
sanket <sanketparekh17@gmail.com>
2025-02-19 14:27:19 +0530

    feat: implement logic for multiple number in string

--- src/string_calculator.ts
+++ src/string_calculator.ts
@@ -2,6 +2,16 @@ export class StringCalculator{
     add(number: string): number {
         if(number == "") return 0;
 
-        return parseInt(number);
+        if(!number.includes(",")){
+            return parseInt(number)
+        }
+
+        let numberArray = number.split(',')
+        let sum = numberArray.reduce((accSum : number, currNumber: string): number=>{
+            accSum += parseInt(currNumber)
+            return accSum
+        },0);
+
+        return sum;
     }
 } 
