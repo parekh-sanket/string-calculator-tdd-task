a7837a9fc9b13eeedd16fa4ae0e113f2e6bf2041
sanket <sanketparekh17@gmail.com>
2025-02-19 15:05:35 +0530

    feat: implement support for diffrent delimiters

--- src/string_calculator.ts
+++ src/string_calculator.ts
@@ -2,12 +2,21 @@ export class StringCalculator{
     add(numbers: string): number {
         if(numbers == "") return 0;
 
-        if(!numbers.includes(",")){
+        if(!numbers.includes(",") && !numbers.startsWith("//")){
             return parseInt(numbers)
         }
 
-        let normalizedNumbers = numbers.replace(/\n/g, ","); 
-        let numbersArray = normalizedNumbers.split(',')
+        let delimiter = ",", numbersStr = numbers
+
+        if(numbers.startsWith("//")){
+            const firstNewLine = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, firstNewLine);
+            numbersStr = numbers.substring(firstNewLine + 1);
+        }
+
+        let normalizedNumbers = numbersStr.replace(/\n/g, delimiter); 
+        let numbersArray = normalizedNumbers.split(delimiter)
+        console.log("numbersArray " , numbersArray)
         let sum = numbersArray.reduce((accSum : number, currNumbers: string): number=>{
             accSum += parseInt(currNumbers)
             return accSum
